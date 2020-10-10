464b4ffacf9f778a7afb21325e8f2afade4df940
jian.zhang <jian.zhang@convertlab.com>
2020-10-10 16:14:37 +0800

    add render function

--- createElement && render.js
+++ createElement && render.js
@@ -41,7 +41,7 @@ function createElement(type, props, ...children) {
         type,
         props: {
             ...props,
-            children: children.map(child => 
+            children: children.map(child =>
                 typeof child === 'object' ?
                     child : createTextElement(child)
             )
@@ -56,6 +56,23 @@ function createTextElement(text) {
             children: []
         }
     }
+}
+
+function render(element, container) {
+    
+    const dom = element.type === "TEXT_ELEMENT" ?
+        document.createTextNode("") :
+        document.createElement(element.type)
+
+
+    const isProperty = key => key !== "children"
+    Object.keys(element.props).filter(isProperty).forEach(name => {
+        dom[name] = element.props[name]
+    })
+
+
+    element.props.children.forEach(item => render(item, dom))
+    container.appendChild(dom)
 }
 const element = (
     <div id="foo">
